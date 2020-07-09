b9c0de79e2967217da5276a098503efe8247df2f
Sam Wilson <sam.wilson@devtank.co.uk>
2020-07-09 16:31:26 +0100

    figured out how to import all the bits and bobs in the package

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-__all__ = ['sig_gen.py', 'spec_ana.py', 'substrate.py', 'vna.py']
+from dt_scpi_lib.sig_gen import scpi_sig_gen, hmct2220, hp8648
+from dt_scpi_lib.spec_ana import agilent_8563, e4440
+from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm
+from dt_scpi_lib.vna import hp8720d
