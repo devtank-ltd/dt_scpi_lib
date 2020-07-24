867b2e98aa4b9e9c4db889da1da0ca9bb201318a
Sam Wilson <sam.wilson@devtank.co.uk>
2020-07-24 10:18:20 +0100

    Initial stab at supporting Tektronix TDS-series oscilloscopes

--- oscilloscope.py
+++ oscilloscope.py
@@ -14,6 +14,39 @@ class oscilloscope_t(object):
     def idn(self):
         return self.gpib.read("*idn?;")
 
+class tektronix_tds(oscilloscope_t):
+
+    channel1 = "CH1"
+    channel2 = "CH2"
+    channel3 = "CH3"
+    channel4 = "CH4"
+    ext = "EXT"
+    ext5 = "EXT5"
+    ext10 = "EXT10"
+
+    rising = "RISe"
+    falling = "FALL"
+
+    def __init__(self, f):
+        self.gpib = f
+
+    def is_channel(self, name):
+        return name in [self.channel1, self.channel2, self.channel3, self.channel3, self.ext, self.ext5, self.ext10]
+
+    def trigger_single(self):
+        self.gpib.write(":SINGle")
+
+    def trigger_force(self):
+        self.gpib.write(":TRIGger:MAIn")
+
+    def trigger_edge(self, source, edge_type):
+        # This is not working, but I'm not going to try and find out why yet.
+        assert(self.is_channel(source))
+        self.gpib.write(":TRIGger:MODe EDGE")
+        self.gpib.write(":TRIGger:EDGe:SOURce %s" % source)
+        self.gpib.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
+
+
 class rigol_ds1000z_t(oscilloscope_t):
     d0 = "D0"
     d1 = "D1"
