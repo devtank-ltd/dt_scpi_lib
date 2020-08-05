b37e8324e84938e6f5de9ad0a79df652062139ef
Sam Wilson <sam.wilson@devtank.co.uk>
2020-08-05 12:22:55 +0100

    don't forget to import the power meter

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from dt_scpi_lib.spec_ana import agilent_8563, e4440
 from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log
 from dt_scpi_lib.vna import hp8720d
 from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds
+from dt_scpi_lib.power_meter import u2020_t
