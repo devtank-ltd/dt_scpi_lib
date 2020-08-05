5136aa117c27ff29a10a5b65beffeb5dbf233233
Sam Wilson <sam.wilson@devtank.co.uk>
2020-08-05 12:17:02 +0100

    First stab at supporting pulsed measurements on the power meter

--- power_meter.py
+++ power_meter.py
@@ -28,17 +28,84 @@ class u2020_t(power_meter_t):
     def __init__(self, gpib):
         self.gpib = gpib
 
+    def pulsed(freq, delay, time):
+        # freq is the frequency in Hz
+        # delay is how much time after the trigger event to start measuring in seconds
+        # time is the duration of the time-gated period
+
+        # Presets the U2020 X-Series to default values.
+        self.gpib.write("SYST:PRES")
+
+        # Sets the frequency.
+        self.gpib.write("SENS:FREQ %u" % freq)
+
+        # Sets the trigger source to external trigger input.
+        self.gpib.write("TRIG:SOUR EXT") # TODO, is this the correct trigger type?
+
+        # On reset, the trigger is configured to happen on the rising edge of a signal, which is exactly what we need
+
+        # Sets the measurement speed to fast mode.
+        self.gpib.write("SENS:MRAT FAST")
+
+        # Sets the buffer size of the U2020 X-Series to 100 to store 100 measurement readings.
+        self.gpib.write("TRIG:COUN 100")
+
+        # Disables auto-zeroing.
+        self.gpib.write("CAL:ZERO:AUTO OFF")
+
+        # Disables auto-calibration.
+        self.gpib.write("CAL:AUTO OFF")
+
+        # Set the delay between the triggered point and the start of the time-gated period.
+        self.gpib.write("SENS:SWE:OFFS:TIME %u" % delay)
+
+        # Set the duration of the time-gated period
+        self.gpib.write("SENS:SWE:TIME %u" % time)
+
+        # Sets the power measurement unit for CALC1 to W.
+        self.gpib.write("UNIT:POW W") # TODO: do we want watts or dB?
+
+        # Sets the data format to REAL.
+        self.gpib.write("FORM REAL")
+
+        # Fetches the reading.
+        return self.gpib.read("FETC?")
+
     def measure():
-        self.gpib.write("SYST:PRES") # Presets the U2020 X-Series to default values.
-        self.gpib.write("SENS:FREQ 50MHz") # Sets the frequency to 50 MHz.
-        self.gpib.write("INIT:CONT ON") # Initiates the trigger sequence.
-        self.gpib.write("UNIT:POW DBM") # Sets the power measurement unit for CALC1 to W.
-        self.gpib.write("FORM REAL") # Sets the data format to REAL.
-        self.gpib.write("CAL:ZERO:AUTO OFF") # Disables auto-zeroing.
-        self.gpib.write("CAL:AUTO OFF") # Disables auto-calibration.
-        self.gpib.write("SENS:AVER:SDET OFF") # Disables step detection.
-        self.gpib.write("SENS:DET:FUNC NORM") # Sets the measurement mode to normal.
-        self.gpib.write("SENS:MRAT FAST") # Sets the measurement speed to fast mode.
-        self.gpib.write("TRIG:COUN 100") # Sets the buffer size of the U2020 X-Series to 100 to store 100 measurement readings.
-        return self.gpib.read("FETC?") # Fetches the reading.
+        # Presets the U2020 X-Series to default values.
+        self.gpib.write("SYST:PRES") 
+
+        # Sets the frequency to 50 MHz.
+        self.gpib.write("SENS:FREQ 50MHz") 
+
+        # Initiates the trigger sequence.
+        self.gpib.write("INIT:CONT ON") 
+
+        # Sets the power measurement unit for CALC1 to W.
+        self.gpib.write("UNIT:POW DBM") 
+
+        # Sets the data format to REAL.
+        self.gpib.write("FORM REAL") 
+
+        # Disables auto-zeroing.
+        self.gpib.write("CAL:ZERO:AUTO OFF") 
+
+        # Disables auto-calibration.
+        self.gpib.write("CAL:AUTO OFF") 
+
+        # Disables step detection.
+        self.gpib.write("SENS:AVER:SDET OFF") 
+
+        # Sets the measurement mode to normal.
+        self.gpib.write("SENS:DET:FUNC NORM") 
+
+        # Sets the measurement speed to fast mode.
+        self.gpib.write("SENS:MRAT FAST") 
+
+        # Sets the buffer size of the U2020 X-Series to 100 to store 100 measurement readings.
+        self.gpib.write("TRIG:COUN 100") 
+
+        # Fetches the reading.
+        return self.gpib.read("FETC?") 
+
 
