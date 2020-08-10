ab24a33e26070d856858b539d566435ce33affc4
Sam Wilson <sam.wilson@devtank.co.uk>
2020-08-10 11:01:42 +0100

    Import power supplies

--- __init__.py
+++ __init__.py
@@ -5,3 +5,4 @@ from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substra
 from dt_scpi_lib.vna import hp8720d
 from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds
 from dt_scpi_lib.power_meter import u2020_t
+from dt_scpi_lib.power_supply import power_supply_t, thurlby_pl330, n6700
