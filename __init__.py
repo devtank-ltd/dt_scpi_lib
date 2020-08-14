e454f8ccbb336b7c9113953eef2c0ebaba6c71fc
Sam Wilson <sam.wilson@devtank.co.uk>
2020-08-14 12:15:44 +0100

    New driver e36300 for Customer bench supply

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ from dt_scpi_lib.spec_ana import agilent_8563, e4440
 from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log
 from dt_scpi_lib.vna import hp8720d
 from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds
-from dt_scpi_lib.power_meter import u2020_t, e36300
-from dt_scpi_lib.power_supply import power_supply_t, thurlby_pl330, n6700
+from dt_scpi_lib.power_meter import u2020_t
+from dt_scpi_lib.power_supply import power_supply_t, thurlby_pl330, n6700, e36300
