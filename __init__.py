a52841bcea688b36ad3c195a3a7512f67e0814f3
Sam Wilson <sam.wilson@devtank.co.uk>
2020-08-17 12:21:06 +0100

    add driver for Customer' other oscilloscope

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from dt_scpi_lib.ieee488 import ieee488_t, scpi_t
 from dt_scpi_lib.sig_gen import fake_sig_gen, scpi_sig_gen, hmct2220, hp8648
 from dt_scpi_lib.spec_ana import agilent_8563, e4440
-from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log, fet_emulator
+from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log
 from dt_scpi_lib.vna import hp8720d
-from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds
+from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds, dsox1024a
 from dt_scpi_lib.power_meter import u2020_t
 from dt_scpi_lib.power_supply import power_supply_t, thurlby_pl330, n6700, e36300
+from dt_scpi_lib.fakes import fet_emulator, fake_customer_dut
--- oscilloscope.py
+++ oscilloscope.py
@@ -49,7 +49,6 @@ class tektronix_tds(oscilloscope_t):
         self.gpib.write("TRIGger:MAIn:EDGE:SOURce %s" % source)
         self.gpib.write("TRIGger:MAIn:EDGE:SLOPe %s" % edge_type)
 
-
 class rigol_ds1000z_t(oscilloscope_t):
     # It's true; this oscilloscope has some SCPI-like language, but actually is not SCPI.
     # And it also does not accept most IEEE-488.2 commands either
@@ -89,6 +88,48 @@ class rigol_ds1000z_t(oscilloscope_t):
         self.gpib.write(":TRIGger:MODe EDGE")
         self.gpib.write(":TRIGger:EDGe:SOURce %s" % source)
         self.gpib.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
+
+    def amplitude(self):
+        return self.gpib.read(":MEASure:VAMPlitude?")
+
+class dsox1204a(oscilloscope_t):
+    # It's true; this oscilloscope has some SCPI-like language, but actually is not SCPI.
+    # And it also does not accept most IEEE-488.2 commands either
+    d0 = "D0"
+    d1 = "D1"
+    d2 = "D2"
+    d3 = "D3" # These go all the way up to 15 but I can't be bothered to type.
+    channel1 = "CHANnel1"
+    channel2 = "CHANnel2"
+    channel3 = "CHANnel3"
+    channel4 = "CHANnel4"
+    ac = "AC"
+
+    rising = "POS"
+    falling = "NEG"
+    rising_falling = "EITHer"
+
+    def __init__(self, f):
+        self.gpib = f
+    
+    def is_channel(self, name):
+        return name in [self.d0, self.d1, self.d2, self.d3, self.channel1, self.channel2, self.channel3, self.channel3, self.ac]
+
+    def trigger_single(self):
+        self.gpib.write(":SINGle")
+
+    def trigger_force(self):
+        self.gpib.write(":TRIGger:FORCe")
+
+    def trigger_edge(self, source, edge_type): 
+        assert(self.is_channel(source))
+        self.gpib.write(":TRIGger:MODe EDGE")
+        self.gpib.write(":TRIGger:EDGe:SOURce %s" % source)
+        self.gpib.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
+    
+    def measure(self, channel):
+        assert(self.is_channel(channel))
+        self.gpib.write(":MEASure:SOURce %s" % channel)
 
     def amplitude(self):
         return self.gpib.read(":MEASure:VAMPlitude?")
