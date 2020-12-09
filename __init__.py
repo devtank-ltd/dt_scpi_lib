d5354b6363b4fb1756e91701a4c59b9ccbff4b50
Sam Wilson <sam.wilson@devtank.co.uk>
2020-12-09 11:29:12 +0000

    cannot import pyusbtmc because it doesn't exist

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from dt_scpi_lib.ieee488 import ieee488_t, scpi_t
 from dt_scpi_lib.sig_gen import fake_sig_gen, scpi_sig_gen, hmct2220, hp8648, smbv100a, smw200a
 from dt_scpi_lib.spec_ana import agilent_8563, e4440
-from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log, excruciating_debug_substrate, pyusbtmc
+from dt_scpi_lib.substrate import prologix_substrate, gpib_device, dummy_substrate, usbtty, usbtmc, socket_comm, log, stderr_log, excruciating_debug_substrate
 from dt_scpi_lib.vna import hp8720d
 from dt_scpi_lib.oscilloscope import oscilloscope_t, rigol_ds1000z_t, tektronix_tds, dsox1204a
 from dt_scpi_lib.power_meter import u2020_t
