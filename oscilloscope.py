c736aff5f705dfac7588695e6caaf2be293b8ad7
Sam Wilson <sam.wilson@devtank.co.uk>
2021-04-28 09:11:46 +0100

    substrate not gpib

--- oscilloscope.py
+++ oscilloscope.py
@@ -9,10 +9,10 @@ from dt_scpi_lib.parameter import *
 class oscilloscope_t(object):
 
     def run():
-        self.gpib.write(":run")
+        self.substrate.write(":run")
 
     def stop():
-        self.gpib.write(":stop")
+        self.substrate.write(":stop")
 
 class tektronix_tds(oscilloscope_t):
 
@@ -35,20 +35,20 @@ class tektronix_tds(oscilloscope_t):
         return name in [self.channel1, self.channel2, self.channel3, self.channel3, self.ext, self.ext5, self.ext10]
 
     def trigger_q(self):
-        self.gpib.read("TRIGger?")
+        self.substrate.read("TRIGger?")
 
     def trigger_single(self):
-        self.gpib.write(":SINGle")
+        self.substrate.write(":SINGle")
 
     def trigger_force(self):
-        self.gpib.write("TRIGger FORCe")
+        self.substrate.write("TRIGger FORCe")
 
     def trigger_edge(self, source, edge_type):
         # This is not working, but I'm not going to try and find out why yet.
         assert(self.is_channel(source))
-        self.gpib.write("TRIGger:MAIn:MODe NORMal")
-        self.gpib.write("TRIGger:MAIn:EDGE:SOURce %s" % source)
-        self.gpib.write("TRIGger:MAIn:EDGE:SLOPe %s" % edge_type)
+        self.substrate.write("TRIGger:MAIn:MODe NORMal")
+        self.substrate.write("TRIGger:MAIn:EDGE:SOURce %s" % source)
+        self.substrate.write("TRIGger:MAIn:EDGE:SLOPe %s" % edge_type)
 
 class rigol_ds1000z_t(oscilloscope_t):
     # It's true; this oscilloscope has some SCPI-like language, but actually is not SCPI.
@@ -75,19 +75,19 @@ class rigol_ds1000z_t(oscilloscope_t):
         return name in [self.d0, self.d1, self.d2, self.d3, self.channel1, self.channel2, self.channel3, self.channel3, self.ac]
 
     def trigger_single(self):
-        self.gpib.write(":SINGle")
+        self.substrate.write(":SINGle")
 
     def trigger_force(self):
-        self.gpib.write(":TFORce")
+        self.substrate.write(":TFORce")
 
     def trigger_edge(self, source, edge_type): 
         assert(self.is_channel(source))
-        self.gpib.write(":TRIGger:MODe EDGE")
-        self.gpib.write(":TRIGger:EDGe:SOURce %s" % source)
-        self.gpib.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
+        self.substrate.write(":TRIGger:MODe EDGE")
+        self.substrate.write(":TRIGger:EDGe:SOURce %s" % source)
+        self.substrate.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
 
     def amplitude(self):
-        return self.gpib.read(":MEASure:VAMPlitude?")
+        return self.substrate.read(":MEASure:VAMPlitude?")
 
 class dsox1204a(oscilloscope_t, ieee488_t):
     # It's true; this oscilloscope has some SCPI-like language, but actually is not SCPI.
@@ -129,61 +129,61 @@ class dsox1204a(oscilloscope_t, ieee488_t):
         # The first byte is an ASCII character '1' thru '9', which is the length of the length, and the next 1 to 9 bytes are the
         # length. So if the block is 100 bytes long, then because 100 is a three-digit number the first four bytes in the block are
         # "3100". This scheme effectively limits the block length to just shy of 10 kilobytes; have I misunderstood something?
-        i = int(self.gpib.get_byte())
+        i = int(self.substrate.get_byte())
         length = 0
         for i in range(0, i):
-            length = length * 10 + int(self.gpib.get_byte())
-        self.gpib.remark("Fetching a %u byte block" % length)
+            length = length * 10 + int(self.substrate.get_byte())
+        self.substrate.remark("Fetching a %u byte block" % length)
 
         for l in range(0, l):
-            yield self.gpib(get_byte())
+            yield self.substrate(get_byte())
 
     def screenshot(self, filename):
-        self.gpib.write(":HARDcopy:INKSaver OFF")
-        self.gpib.write(":DISPlay:DATA? PNG, COLor")
-        data = self.gpib.readblock()
+        self.substrate.write(":HARDcopy:INKSaver OFF")
+        self.substrate.write(":DISPlay:DATA? PNG, COLor")
+        data = self.substrate.readblock()
         with open(filename, "w+b") as scrshot:
             scrshot.write(data)
 
     def channel_autoscale(self, channel):
         assert(self.is_channel(channel))
-        self.gpib.write(":AUToscale %s" % channel)
+        self.substrate.write(":AUToscale %s" % channel)
 
     def channel_scale(self, channel, scale):
         assert(self.is_channel(channel))
-        self.gpib.write(":%s:SCALe %fV" % (channel, scale))
+        self.substrate.write(":%s:SCALe %fV" % (channel, scale))
 
     def channel_offset(self, channel, offset):
         assert(self.is_channel(channel))
-        self.gpib.write(":%s:OFFSet %fV" % (channel, offset))
+        self.substrate.write(":%s:OFFSet %fV" % (channel, offset))
 
     def trigger_single(self):
-        self.gpib.write(":SINGle")
+        self.substrate.write(":SINGle")
 
     def trigger_force(self):
-        self.gpib.write(":TRIGger:FORCe")
+        self.substrate.write(":TRIGger:FORCe")
 
     def trigger_auto(self):
-        self.gpib.write(":TRIGger:SWEep AUTO")
+        self.substrate.write(":TRIGger:SWEep AUTO")
 
     def trigger_normal(self):
-        self.gpib.write(":TRIGger:SWEep NORMal")
+        self.substrate.write(":TRIGger:SWEep NORMal")
 
     def trigger_edge(self, source, edge_type): 
         assert(self.is_channel(source))
-        self.gpib.write(":TRIGger:MODe EDGE")
-        self.gpib.write(":TRIGger:EDGe:SOURce %s" % source)
-        self.gpib.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
+        self.substrate.write(":TRIGger:MODe EDGE")
+        self.substrate.write(":TRIGger:EDGe:SOURce %s" % source)
+        self.substrate.write(":TRIGger:EDGe:SLOPe %s" % edge_type)
     
     def measure(self, channel):
         assert(self.is_channel(channel))
-        self.gpib.write(":MEASure:SOURce %s" % channel)
+        self.substrate.write(":MEASure:SOURce %s" % channel)
 
     def amplitude(self, channel=None):
         src = ("" if channel is None else channel)
-        self.gpib.write(":MEASure:SOURce %s" % src)
-        self.gpib.write(":MEASure:VAMPlitude %s" % src)
-        return self.measurement_parse(self.gpib.read(":MEASure:VAMPlitude? %s" % src))
+        self.substrate.write(":MEASure:SOURce %s" % src)
+        self.substrate.write(":MEASure:VAMPlitude %s" % src)
+        return self.measurement_parse(self.substrate.read(":MEASure:VAMPlitude? %s" % src))
 
     def system_error(self):
-        return self.gpib.read("SYSTem:ERRor?")
+        return self.substrate.read("SYSTem:ERRor?")
