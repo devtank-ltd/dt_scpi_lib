fbe4420c7a066dbebae7e5433f52093374d11156
Sam Wilson <sam.wilson@devtank.co.uk>
2021-04-28 13:50:09 +0100

    Add parameter for PSU output enable

--- power_supply.py
+++ power_supply.py
@@ -90,6 +90,7 @@ class e36300(scpi_t):
         self.chan = chan
         self.mcurrent_limit = 0;
         self.mvoltage = 0;
+        self.output_enable = memoizing_parameter_t(self, lambda en: "outp %d @%d;" % (1 if en else 0, chan), getter="outp? @%d" % chan)
 
     @property
     def voltage(self):
